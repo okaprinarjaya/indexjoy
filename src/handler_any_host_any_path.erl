236d5ed35b0338d8b0ed41ec7760fe290a0220c9
Oka Prinarjaya <okaprinarjaya@gmail.com>
2019-11-09 22:59:10 +0700

    Enhance Bing's bot detection

--- src/handler_any_host_any_path.erl
+++ src/handler_any_host_any_path.erl
@@ -27,13 +27,21 @@ whos_coming_detection_by_user_agent(RequestFacilitator) ->
     <<"DuckDuckBot/1.0; (+http://duckduckgo.com/duckduckbot.html)">>,
     <<"Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)">>,
     <<"Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)">>,
-    <<"Mozilla/5.0 (Windows Phone 8.1; ARM; Trident/7.0; Touch; rv:11.0; IEMobile/11.0; NOKIA; Lumia 530) like Gecko (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)">>
+    <<"Mozilla/5.0 (Windows Phone 8.1; ARM; Trident/7.0; Touch; rv:11.0; IEMobile/11.0; NOKIA; Lumia 530) like Gecko (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)">>,
+    <<"Mozilla/5.0 (Windows Phone 8.1; ARM; Trident/7.0; Touch; rv:11.0; IEMobile/11.0; NOKIA; Lumia 530) like Gecko BingPreview/1.0b">>,
+    <<"Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53 BingPreview/1.0b">>
   ],
-  UserAgent = cowboy_req:header(<<"user-agent">>, RequestFacilitator),
 
-  case lists:member(UserAgent, SearchEngineCrawlerBotList) of
-    true -> 'search-engine-crawler';
-    false -> 'client-browser'
+  case cowboy_req:header(<<"user-agent">>, RequestFacilitator) of
+    undefined ->
+      io:format("USER AGENT UNDEFINED~n"),
+      'search-engine-crawler';
+    UserAgent ->
+      io:format("USER AGENT: ~p~n", [UserAgent]),
+      case lists:member(UserAgent, SearchEngineCrawlerBotList) of
+        true -> 'search-engine-crawler';
+        false -> 'client-browser'
+      end
   end.
 
 handle_client_browser(RequestFacilitator, State) ->
